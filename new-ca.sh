9a64a91060f5be9a050825c8909728a067a8af43
Andrew Benton <andrew@benton.io>
2026-04-29 23:17:09 -0700

    Update openssl invocations for BSD compatibility

--- new-ca.sh
+++ new-ca.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-openssl req -x509 -newkey rsa:4096 -sha256 -keyout "$CA_NAME.combined.pem" -noenc \
+openssl req -x509 -newkey rsa:4096 -sha256 -keyout "$CA_NAME.combined.pem" -nodes \
     -subj "/CN=$CA_NAME" -days 3650 | tee "$CA_NAME.cert.pem" >> "$CA_NAME.combined.pem"
