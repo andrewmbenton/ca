2c639eb77beb14fc3b1a0e2ed0540e531fbe2c28
Andrew Benton <andrew@benton.io>
2026-08-08 16:24:27 -0700

    Update new-ca.sh to add typical default extensions

--- new-ca.sh
+++ new-ca.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 openssl req -x509 -newkey rsa:4096 -sha256 -keyout "$CA_NAME.combined.pem" -nodes \
+    -addext "basicConstraints=critical,CA:TRUE" \
+    -addext "keyUsage=critical,keyCertSign,cRLSign" \
     -subj "/CN=$CA_NAME" -days 3650 | tee "$CA_NAME.cert.pem" >> "$CA_NAME.combined.pem"
